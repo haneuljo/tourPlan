15d222d56c761954656125119a5deb3713ee301e
KimTaeHun <xogns14@hanmail.net>
2015-10-02 17:24:44 +0900

    ??????

--- TourPlan/src/main/java/com/tour/dto/PlanDTO.java
+++ TourPlan/src/main/java/com/tour/dto/PlanDTO.java
@@ -4,6 +4,49 @@ public class PlanDTO {
 	
 	int planNum, groupNum, contentid, contenttypeid;
 	String content, startDate, endDate;
+	public int getPlanNum() {
+		return planNum;
+	}
+	public void setPlanNum(int planNum) {
+		this.planNum = planNum;
+	}
+	public int getGroupNum() {
+		return groupNum;
+	}
+	public void setGroupNum(int groupNum) {
+		this.groupNum = groupNum;
+	}
+	public int getContentid() {
+		return contentid;
+	}
+	public void setContentid(int contentid) {
+		this.contentid = contentid;
+	}
+	public int getContenttypeid() {
+		return contenttypeid;
+	}
+	public void setContenttypeid(int contenttypeid) {
+		this.contenttypeid = contenttypeid;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	
 	
 	
 }
